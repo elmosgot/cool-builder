669344ddd8d929c7f1f260dfe86808bd2e598f83
Elwin Schotanus <elwin@ingenium-studios.com>
2016-02-05 20:49:32 -0600

    Added retrieving of dirs and started handling post

--- module/Moduler/src/Moduler/Controller/ModulerController.php
+++ module/Moduler/src/Moduler/Controller/ModulerController.php
@@ -7,8 +7,45 @@ use Zend\View\Model\ViewModel;
 use Moduler\Form\ModulerForm;
 
 class ModulerController extends AbstractActionController {
+	protected $templatesDir;
+	protected $projectsDir;
+
 	public function indexAction() {
-		$form = new ModulerForm();
+		$data = array(
+			'templates_dir' => $this->getTemplatesDir(),
+			'projects_dir' => $this->getProjectsDir()
+		);
+		$form = new ModulerForm( 'moduler', $data );
+		$request = $this->getRequest();
+		if( $request->isPost() ) {
+			$form->setData( $request->getPost() );
+			if( $form->isValid() ) {
+				echo "Creating new module...";
+				echo "<pre>";
+				print_r( $form->getData() );
+				die();
+			}
+		}
 		return new ViewModel( array( 'form' => $form ) );
+	}
+	protected function getTemplatesDir() {
+		if( !$this->templatesDir ) {
+			$config = $this->getServiceLocator()->get('config');
+			$this->templatesDir = $config['moduler']['templates_dir'];
+		}
+		if( !is_dir( $this->templatesDir ) ) {
+			throw new \Exception( 'Template dir not found!' );
+		}
+		return $this->templatesDir;
+	}
+	protected function getProjectsDir() {
+		if( !$this->projectsDir ) {
+			$config = $this->getServiceLocator()->get('config');
+			$this->projectsDir = $config['projects_dir'];
+		}
+		if( !is_dir( $this->projectsDir ) ) {
+			throw new \Exception( 'Projects dir not found!' );
+		}
+		return $this->projectsDir;
 	}
 }
