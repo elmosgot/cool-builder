3f47775e89a8ec76706f3c260eed2174a25e3984
Elwin Schotanus <elwin@ingenium-studios.com>
2016-02-05 22:12:24 -0600

    Implemented module builder based on old script

---
+++ module/Moduler/src/Moduler/Model/Builder.php
@@ -0,0 +1,91 @@
+<?php
+namespace Moduler\Model;
+
+class Builder {
+	protected $moduler;
+	protected $templatesDir;
+	protected $log;
+
+	public function __construct( $moduler, $templatesDir, &$log ) {
+		$this->moduler = $moduler;
+		$this->templatesDir = $templatesDir;
+		$this->log = $log;
+	}
+	public function buildModule() {
+		$files = $this->readTemplate( $this->getTemplateDir() );
+		$replaceContent = array();
+		$replaceContent[] = $this->getName();
+		$lowerName = $this->getName( true );
+		$replaceContent[] = $lowerName;
+		$replaceContent[] = $this->moduler->table;
+		$replaceContent[] = $this->moduler->unit;
+		$this->createStructure( $this->moduler->getModuleDir() . DIRECTORY_SEPARATOR, $files, array( $this->getTemplateDir() => '', 'template' => $this->getName( true ), 'Template' => $this->getName() ), $replaceContent );
+	}
+	protected function readTemplate( $dir = "template/Module/" ) {
+		$files = array();
+		if( $handle = opendir( $dir ) ) {
+			while( ( $file = readdir( $handle ) ) !== false ) {
+				if( !preg_match( '/^[\.]+$/', $file ) ) {
+					$obj = new \stdClass();
+					$obj->name = $dir . $file;
+					if( is_file( $dir . $file ) ) {
+						$obj->is_file = 1;
+						$files[] = $obj;
+					} else if( is_dir( $dir . $file ) ) {
+						$obj->is_file = 0;
+						$obj->files = $this->readTemplate( $dir . $file . "/" );
+						$files[] = $obj;
+					}
+				}
+			}
+		}
+		return $files;
+	}
+	// Directory structure function
+	protected function createStructure( $dest, $files, $rewritePath, $replaceContent ) {
+		if( !file_exists( $dest ) ) {
+			$this->log[] = "<b>Making folder $dest</b>\n\n";
+			mkdir( $dest );
+		}
+		foreach( $files as $file ) {
+			$destName = $file->name;
+			foreach( $rewritePath as $replace => $with ) {
+				$destName = str_replace( $replace, $with, $destName );
+			}
+			$moduleObj = $dest . $destName;
+			if( $file->is_file ) {
+				$this->log[] = "copy( " . $file->name . ", " . $moduleObj . ");\n";
+				copy( $file->name, $moduleObj );
+				$templateFile = $this->read( $moduleObj );
+				$replace = '"' . implode( '", "', $replaceContent ) . '"';
+				$moduleFile = '';
+				eval( "\$moduleFile = sprintf( \$templateFile, " . $replace . " );" );
+				$this->write( $moduleObj, $moduleFile );
+			} else {
+				if( !file_exists( $moduleObj ) ) {
+					$this->log[] = "\n<b>Making folder $moduleObj</b>\n";
+					mkdir( $moduleObj );
+				}
+				$this->createStructure( $dest, $file->files, $rewritePath, $replaceContent );
+			}
+		}
+	}
+	// File helper functions
+	protected function read( $file ) {
+		$handle = fopen( $file, "r" );
+		$data = fread( $handle, filesize( $file ) );
+		fclose( $handle );
+		return $data;
+	}
+	protected function write( $file, $data ) {
+		$handle = fopen( $file, "w" );
+		fwrite( $handle, $data );
+		fclose( $handle );
+	}
+	protected function getName( $lowerCase = false ) {
+		return $lowerCase ? strtolower( $this->moduler->name ) : $this->moduler->name;
+	}
+	protected function getTemplateDir() {
+		return $this->templatesDir . DIRECTORY_SEPARATOR . $this->moduler->template;
+	}
+}
