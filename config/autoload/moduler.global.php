c19808dd7f3354617f0314e7fdf140f56b2e33e7
Elwin Schotanus <elwin@ingenium-studios.com>
2016-02-08 16:34:28 -0600

    Fixed template and added template2

--- config/autoload/moduler.global.php
+++ config/autoload/moduler.global.php
@@ -2,5 +2,13 @@
 return array(
 	'moduler' => array(
 		'templates_dir' => 'data/templates/moduler'
+	),
+	'template1' => array(
+		'name' => 'Frontend & Unit',
+		'description' => 'ZF2 Module with frontend routes and PHPUnit testing.'
+	),
+	'template2' => array(
+		'name' => 'Backend logic',
+		'description' => 'ZF2 Module for backend logic.'
 	)
 );
