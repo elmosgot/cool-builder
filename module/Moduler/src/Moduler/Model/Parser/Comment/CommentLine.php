39137938bd9f6c3274060bb0fcc65e80c61a893c
Elwin Schotanus <elwin@ingenium-studios.com>
2016-02-08 20:54:40 -0600

    Fixed comment line end detection

--- module/Moduler/src/Moduler/Model/Parser/Comment/CommentLine.php
+++ module/Moduler/src/Moduler/Model/Parser/Comment/CommentLine.php
@@ -2,11 +2,12 @@
 namespace Moduler\Model\Parser\Comment;
 
 use Moduler\Model\Parser\Raw;
+use Moduler\Model\Parser\Regex;
 
 class CommentLine extends Raw {
 	public function __construct( $code, $vector ) {
 		parent::__construct( $code, $vector, 'CommentLine' );
 		$this->parsable = false;
-		$this->pattern = array( '//', '\n' );
+		$this->pattern = array( '//', new Regex( '(\r\n|\n|\r)', 1 ) );
 	}
 }
